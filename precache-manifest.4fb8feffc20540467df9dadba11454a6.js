ef5d9605b43a0327f39abc35ee7e96ff9b3e3176
Igor Suleymanov <radiohead@users.noreply.github.com>
2020-04-20 15:55:58 +0300

    Initial commit

---
+++ precache-manifest.4fb8feffc20540467df9dadba11454a6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669d767a44e45b4676409e99906e90fe",
+    "url": "/browser-client-example/index.html"
+  },
+  {
+    "revision": "b219d105c41b6cd059b4",
+    "url": "/browser-client-example/static/js/2.120d4d27.chunk.js"
+  },
+  {
+    "revision": "eb49ef04e252fa8fb06d",
+    "url": "/browser-client-example/static/js/main.2c18a121.chunk.js"
+  },
+  {
+    "revision": "0538a0f11cdb1817076c",
+    "url": "/browser-client-example/static/js/runtime-main.fe82d3c9.js"
+  }
+]);
